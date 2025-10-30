20296c1fc56ceac14107ca857f5e22b3fa81e340
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-10-30 12:43:25 +0100

    Update 008_Set_and_Map_Collections.kt

--- #01_KOTLINE_BASICS/008_Set_and_Map_Collections.kt
+++ #01_KOTLINE_BASICS/008_Set_and_Map_Collections.kt
@@ -5,15 +5,21 @@ fun main(){
     // if you use MutableSet you can read and Write on the set
     val set1:MutableSet<Int> = mutableSetOf(1,2,3,1,4)
     set1.add(0)
-    println(set1.joinToString())
+    println(set1.joinToString()) // 1, 2, 3, 4, 0
 
 
     // if you use Set you can only read from the set
     val set2:Set<Int> = setOf(1,2,3,1,4)
-    println(set2.joinToString())
+    println(set2.joinToString()) // 1, 2, 3, 4
 
 
     // you can create a map
     val tableMap:Map<Int,String> =mutableMapOf(Pair(1,"java"),Pair(2,"kotlin"),Pair(3,"Swift"))
-    println(tableMap)
+    println(tableMap) // {1=java, 2=kotlin, 3=Swift}
+
+    
+    val tableMap2:MutableMap<Int, String> = mutableMapOf(1 to "C++",2 to "C#",3 to "Kotlin")
+    tableMap2[5]="Java"
+    println(tableMap2)
 }
+
