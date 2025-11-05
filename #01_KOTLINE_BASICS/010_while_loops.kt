cf418883369d464284450544781eb3188058f30c
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-11-05 17:32:09 +0100

    Create 010_while_loops.kt

---
+++ #01_KOTLINE_BASICS/010_while_loops.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var counter=0
+    while(counter<=5){
+        println(counter)
+        counter++
+    }
+
+    println()
+
+    var index=5
+    do{
+        println(index)
+        index--
+    }while(index>0)
+}
